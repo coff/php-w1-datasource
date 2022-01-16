25fee376eb65b2f9310cf5f6e8d8e5f3d1f43674
Krzysztof Kubanek <krzysztof.kubanek@gmail.com>
2022-01-16 14:54:47 +0100

    code cleanups; namespace fixes

--- src/W1FileDataSource.php
+++ src/W1FileDataSource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Coff\OneWire\DataSource;
+namespace Coff\DataSource\W1;
 
 use Coff\DataSource\Exception\DataSourceException;
 
--- src/W1ServerDataSource.php
+++ src/W1ServerDataSource.php
@@ -1,26 +1,14 @@
 <?php
 
-namespace Coff\OneWire\DataSource;
+namespace Coff\DataSource\W1;
 
 use Coff\OneWire\Client\W1Client;
 
 class W1ServerDataSource extends W1DataSource
 {
-
-    /**
-     * @var W1Client
-     */
-    protected $client;
-
     public function __construct($id)
     {
         $this->id = $id;
-    }
-
-    public function setClient(W1Client $client) {
-        $this->client = $client;
-
-        return $this;
     }
 
     public function request()
